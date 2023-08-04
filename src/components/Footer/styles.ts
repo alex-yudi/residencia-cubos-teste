95f0be1890338609e3de9f0db387a0cbb4041142
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-08-04 13:57:36 -0300

    fix: Correção da estilização do Footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,30 +3,24 @@ import styled from 'styled-components'
 export const FooterContainer = styled.footer`
     display: flex;
     justify-content: center;
-    align-self: center;
 
-    width: 1110px;
-    height: 27px;
-    padding: 7px 216px;
-    border-radius: 8px;
+    min-width: 55rem;
+    height: 1.6875rem;
+    padding: 0.438rem 13.5rem;
+    margin-top: 0.625rem;
+    border-radius: 0.5rem;
 
 
     background: ${(props) => props.theme['footerColor']};
 
-    text-align: center;
-    font-family: Rubik;
     font-size: 0.75rem;
-    font-style: normal;
-    font-weight: 400;
     line-height: normal;
 `
 
 export const LinkFooter = styled.a`
     text-align: center;
-    font-family: Rubik;
     font-size: 0.75rem;
     font-style: normal;
-    font-weight: 400;
     line-height: normal;
 
     color: ${(props) => props.theme['linkFontColor']};
