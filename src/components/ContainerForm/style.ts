00932311a4738f21c4ef23e006226a255d37f6a0
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-08-04 16:31:20 -0300

    style: retirado styled components que não são utilizados no component ContainerForm

--- src/components/ContainerForm/style.ts
+++ src/components/ContainerForm/style.ts
@@ -29,32 +29,7 @@ export const ContainerInputs = styled.div`
     display: flex;
     flex-direction: column;
 
-    margin-bottom: 1.30rem
-`
-
-export const InputForm = styled.input`
-    width: 13.0625rem;
-    height: 2rem;
-    padding-left: 1rem;
-    margin-bottom: 0.81rem;
-
-    border-radius: 0.5rem;
-    border: 1px solid #C5C6CA;
-
-    &::placeholder {
-        color: #999;
-        font-size: 0.75rem;
-    }
-`
-
-export const LinkForgotPassword = styled.a`
-    font-size: 0.5625rem;
-    color: #2E7BB4;
-
-    &:hover {
-        cursor: pointer;
-        text-decoration: underline;
-    }
+    margin-bottom: 0.5rem;
 `
 
 export const Button = styled.button`
@@ -64,7 +39,6 @@ export const Button = styled.button`
     padding: 0.1875rem 0.6875rem;
     justify-content: center;
     align-items: center;
-    gap: 0.625rem;
 
     border-radius: 0.5rem;
     border: 1px solid #2E7BB4;
